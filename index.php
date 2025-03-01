7ad97dd26af7d508715665e1cea5ef9885215a1e
Sagar Saini <74305919+SAGAR97619@users.noreply.github.com>
2025-03-01 21:22:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection
-$db = pg_connect("host=localhost dbname=bug_tracking_system user=postgres password=root");
+$db = pg_connect("host=db.rjvjfpamhjrzpjwulbmf.supabase.co dbname=bug_tracking_system user=postgres password=postgres");
 
 if (!$db) {
     die("Error in connection: " . pg_last_error());
